04de69a9c4ccb82fc293bb67067f2f78aeb3e322
Phong Ho <phongho@Phongs-MacBook-Pro.local>
2021-11-01 19:08:03 +0700

    adding a list of recommended movie(similar movies) for each movide page and basic styling

--- src/Pages/MoviePage.js
+++ src/Pages/MoviePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Card } from 'react-bootstrap';
+import { Simulate } from 'react-dom/cjs/react-dom-test-utils.production.min';
 import {useParams} from "react-router-dom";
 
 const MoviePage = () => {
@@ -7,6 +8,7 @@ const MoviePage = () => {
     const API_KEY = "27557e4d1fe2bc4e3586b69c35f06379";
 
     const [movie, setMovie] = useState({})
+    const [similarMovies, setSimilarMovies] = useState([]);
     const params = useParams();
     const { id } = params;
 
@@ -21,10 +23,21 @@ const MoviePage = () => {
         }
         fetchData()
     }, [id])
+
+    useEffect(() => {
+        const getSimilarData = async () => {
+            const url2 = `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${API_KEY}&language=en-US&page=1`;
+            const res2 = await fetch(url2);
+            const result2 = await res2.json();
+            setSimilarMovies(result2.results);
+        }
+        getSimilarData()
+    }, [id]);
+    // console.log(similarMovies);
     return (
         <div>
             <Card className="card">
-                <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} style={{width:"352px"}} atl={movie.title}/>
+                <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} style={{width:"352px"}} alt={movie.title}/>
                 <Card.Body>
                     <Card.Title className="card-title">{movie.title}</Card.Title>
                     <Card.Subtitle></Card.Subtitle>
@@ -32,8 +45,26 @@ const MoviePage = () => {
                     <Card.Text>Rating: {movie.vote_average}</Card.Text>
                 </Card.Body>
             </Card>
+            <div className="similarMovies">
+            <h3>You may also like</h3>
+            <div className="similarMovies-container" style={{display:"flex", flexWrap:"wrap", justifyContent:"space-around"}}>
+            {similarMovies && similarMovies.map((e) => {
+                return (
+                    <Card style={{ width: '18rem', marginBottom:"1rem" }}>
+                        <Card.Img variant="top" src={`https://image.tmdb.org/t/p/original${e?.poster_path}`} />
+                        <Card.Body>
+                            <Card.Title>{e?.title || e?.original_title}</Card.Title>
+                            <Card.Text>
+                                Viewers' Rating: {e?.vote_average}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                )
+            })}
+            </div>
+            </div>
         </div>
     )
 }
 
-export default MoviePage
+export default MoviePage;
